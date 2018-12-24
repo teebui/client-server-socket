225e41e75cf61f69e908b764e167282eabc0ebcf
Tuan-Anh BUI <tuananh.bui@euranova.eu>
2018-12-24 23:07:36 +0100

    Phase 1 passed

--- src/main/java/libs/ClientSocketHandler.java
+++ src/main/java/libs/ClientSocketHandler.java
@@ -1,36 +1,55 @@
 package libs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
-import java.util.UUID;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.Executors;
 
 import static java.lang.String.*;
 
 public class ClientSocketHandler {
     public static final String HI_I_M = "HI, I'M ";
+
     private Socket clientSocket;
-    private UUID sessionID;
+    private Session session;
+
 
     public ClientSocketHandler(Socket clientSkt) {
         clientSocket = clientSkt;
-        sessionID = UUID.randomUUID();
+        session = new Session();
+
     }
 
     public void handle() {
-        System.out.println("Start handling client with session ID " + sessionID);
+        System.out.println("Start handling client with session ID " + session.getSessionID());
         PrintWriter out = null;
-        BufferedReader in = null;
+        BufferedReader in;
         String request, response;
         try {
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            clientSocket.setSoTimeout(30000);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+        try {
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+            String connectionStartText = format("HI, I'M %s", session.getSessionID());
+            out.println(connectionStartText);
+            System.out.println("S: " + connectionStartText);
+
             while ((request = in.readLine()) != null) {
-                response = getResponse(request);
+                System.out.println("C: " + request);
+                response = getResponse(request, session);
+                System.out.println("S: " + response);
                 out.println(response);
             }
+        } catch (SocketTimeoutException e) {
+            session.terminate();
+            out.println(format("BYE %s, WE SPOKE FOR %d MS", session.getClientName(), session.getDuration()));
+        } catch (InterruptedIOException e) {
+            session.terminate();
+            out.println(format("BYE %s, WE SPOKE FOR %d MS", session.getClientName(), session.getDuration()));
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -40,14 +59,15 @@ public class ClientSocketHandler {
         }
     }
 
-    private String getResponse(final String request) {
-        String clientName = request.replace(HI_I_M, "");
+    private String getResponse(final String request, final Session session) {
+        if (request.startsWith(HI_I_M)) {
+            final String clientID = request.replace(HI_I_M, "");
+            session.setClientName(clientID);
 
-        if (request.length() == 0) {
-            return format("HI, I'M %s", sessionID);
-        }
-        else if (request.startsWith(HI_I_M)) {
-            return format("HI %s", clientName);
+            return format("HI %s", clientID);
+        } else if (request.equals("BYE MATE!")) {
+            session.terminate();
+            return format("BYE %s, WE SPOKE FOR %d MS", session.getClientName(), session.getDuration());
         }
 
         return "SORRY, I DIDN'T UNDERSTAND THAT";
---
+++ src/main/java/libs/Session.java
@@ -0,0 +1,47 @@
+package libs;
+
+import java.util.UUID;
+
+public class Session {
+    private final UUID sessionID;
+    private String clientName;
+    private final long startTime;
+    private long endTime = -1;
+
+    public Session() {
+        sessionID = UUID.randomUUID();
+        startTime = System.currentTimeMillis();
+    }
+
+    public UUID getSessionID() {
+        return sessionID;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void terminate() {
+        endTime = System.currentTimeMillis();
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public boolean isActive() {
+        return endTime == -1;
+    }
+
+    public long getDuration() {
+        return endTime - startTime;
+    }
+
+    public void setClientName(String name) {
+        this.clientName = name;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+}
