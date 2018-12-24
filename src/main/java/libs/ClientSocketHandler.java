d0fd996e1f173ce8ff4734593bfd258ef9b7ee06
Tuan-Anh BUI <tuananh.bui@euranova.eu>
2018-12-24 19:48:34 +0100

    Say hi to server, and server responds

--- src/main/java/libs/ClientSocketHandler.java
+++ src/main/java/libs/ClientSocketHandler.java
@@ -1,11 +1,16 @@
 package libs;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.UUID;
 
+import static java.lang.String.*;
+
 public class ClientSocketHandler {
+    public static final String HI_I_M = "HI, I'M ";
     private Socket clientSocket;
     private UUID sessionID;
 
@@ -17,10 +22,15 @@ public class ClientSocketHandler {
     public void handle() {
         System.out.println("Start handling client with session ID " + sessionID);
         PrintWriter out = null;
+        BufferedReader in = null;
+        String request, response;
         try {
             out = new PrintWriter(clientSocket.getOutputStream(), true);
-            out.println(("Hi, i'm " + sessionID).toUpperCase());
-
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            while ((request = in.readLine()) != null) {
+                response = getResponse(request);
+                out.println(response);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -28,5 +38,18 @@ public class ClientSocketHandler {
                 out.close();
             }
         }
+    }
+
+    private String getResponse(final String request) {
+        String clientName = request.replace(HI_I_M, "");
+
+        if (request.length() == 0) {
+            return format("HI, I'M %s", sessionID);
+        }
+        else if (request.startsWith(HI_I_M)) {
+            return format("HI %s", clientName);
+        }
+
+        return "SORRY, I DIDN'T UNDERSTAND THAT";
     }
 }
