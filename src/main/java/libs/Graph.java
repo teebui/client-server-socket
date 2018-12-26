4e1fe4eefbd9d6b217280dc4201996efa77376f5
Tuan-Anh BUI <tuananh.bui@euranova.eu>
2018-12-26 18:51:58 +0100

    Use manual assertion to check if a node exists

--- src/main/java/libs/Graph.java
+++ src/main/java/libs/Graph.java
@@ -1,7 +1,7 @@
 package libs;
 
 import org.jgrapht.GraphPath;
-import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
+import org.jgrapht.alg.interfaces.ShortestPathAlgorithm.SingleSourcePaths;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.DirectedWeightedMultigraph;
@@ -12,10 +12,12 @@ public class Graph {
     private DirectedWeightedMultigraph<String, DefaultWeightedEdge> graph;
 
     public Graph() {
+
         graph = new DirectedWeightedMultigraph<>(DefaultWeightedEdge.class);
     }
 
     public synchronized void addNode(final String nodeName) throws NodeAlreadyExistsException {
+
         if (!graph.addVertex(nodeName)) {
             throw new NodeAlreadyExistsException();
         }
@@ -27,18 +29,18 @@ public class Graph {
         }
     }
 
-    public synchronized void addEdge(final String source, final String target, int weight)
+    public synchronized void addEdge(final String source, final String target, final int weight)
             throws NodeNotFoundException {
-        try {
-            DefaultWeightedEdge edge = graph.addEdge(source, target);
-            graph.setEdgeWeight(edge, weight);
 
-        } catch (IllegalArgumentException e) {
-            throw new NodeNotFoundException();
-        }
+        assertNodeExists(source);
+        assertNodeExists(target);
+
+        DefaultWeightedEdge edge = graph.addEdge(source, target);
+        graph.setEdgeWeight(edge, weight);
     }
 
     public synchronized void removeEdge(final String source, final String target) throws NodeNotFoundException {
+
         assertNodeExists(source);
         assertNodeExists(target);
 
@@ -48,6 +50,7 @@ public class Graph {
     }
 
     public synchronized int getShortestPath(final String source, final String target) throws NodeNotFoundException {
+
         assertNodeExists(source);
         assertNodeExists(target);
 
@@ -57,10 +60,10 @@ public class Graph {
 
     public synchronized String findNodesCloserThan(final int weight, final String node)
             throws NodeNotFoundException {
+
         assertNodeExists(node);
 
-        ShortestPathAlgorithm.SingleSourcePaths<String, DefaultWeightedEdge> paths = new DijkstraShortestPath<>(graph)
-                .getPaths(node);
+        SingleSourcePaths<String, DefaultWeightedEdge> paths = new DijkstraShortestPath<>(graph).getPaths(node);
 
         return paths
                 .getGraph()
@@ -72,9 +75,8 @@ public class Graph {
                 .collect(Collectors.joining(","));
     }
 
+    private synchronized void assertNodeExists(final String node) throws NodeNotFoundException {
 
-
-    private void assertNodeExists(String node) throws NodeNotFoundException {
         if (!graph.containsVertex(node))
             throw new NodeNotFoundException();
     }
