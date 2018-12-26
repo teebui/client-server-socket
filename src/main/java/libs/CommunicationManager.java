77475a7f8a8fabb86f8853c9219cc5c6e90e0788
Tuan-Anh BUI <tuananh.bui@euranova.eu>
2018-12-26 22:06:40 +0100

    Rename all constants

--- src/main/java/libs/CommunicationManager.java
+++ src/main/java/libs/CommunicationManager.java
@@ -4,58 +4,67 @@ import static java.lang.String.format;
 
 public class CommunicationManager {
 
-    private static final String HI_I_M = "HI, I'M ";
-    private static final String HI = "HI %s";
-    private static final String SERVER_BYE = "BYE %s, WE SPOKE FOR %d MS";
-    private static final String CLIENT_BYE = "BYE MATE!";
-    private static final String SERVER_UNKNOWN_REQUEST = "SORRY, I DIDN'T UNDERSTAND THAT";
-    private static final String ERROR_NODE_ALREADY_EXISTS = "ERROR: NODE ALREADY EXISTS";
-    private static final String NODE_ADDED = "NODE ADDED";
-    private static final String ERROR_NODE_NOT_FOUND = "ERROR: NODE NOT FOUND";
+    // List of client commands
+    private static final String CMD_CLIENT_GREETING = "HI, I'M ";
+    private static final String CMD_CLIENT_BYE = "BYE MATE!";
     private static final String CMD_REMOVE_EDGE = "REMOVE EDGE ";
-    private static final String RSP_EDGE_REMOVED = "EDGE REMOVED";
     private static final String CMD_ADD_NODE = "ADD NODE ";
     private static final String CMD_ADD_EDGE = "ADD EDGE ";
-    private static final String RSP_EDGE_ADDED = "EDGE ADDED";
     private static final String CMD_REMOVE_NODE = "REMOVE NODE ";
-    private static final String RSP_NODE_REMOVED = "NODE REMOVED";
     private static final String CMD_SHORTEST_PATH = "SHORTEST PATH ";
     private static final String CMD_CLOSER_THAN = "CLOSER THAN ";
 
-    public static String clientSaysGoodBye() {
-        return CLIENT_BYE;
+    // List of server responses
+    private static final String RSP_SERVER_INTRO = "HI, I'M %s";
+    private static final String RSP_SERVER_GREETING = "HI %s";
+    private static final String RSP_SERVER_BYE = "BYE %s, WE SPOKE FOR %d MS";
+    private static final String RSP_UNKNOWN_REQUEST = "SORRY, I DIDN'T UNDERSTAND THAT";
+    private static final String RSP_ERROR_NODE_ALREADY_EXISTS = "ERROR: NODE ALREADY EXISTS";
+    private static final String RSP_NODE_ADDED = "NODE ADDED";
+    private static final String RSP_ERROR_NODE_NOT_FOUND = "ERROR: NODE NOT FOUND";
+    private static final String RSP_EDGE_REMOVED = "EDGE REMOVED";
+    private static final String RSP_EDGE_ADDED = "EDGE ADDED";
+    private static final String RSP_NODE_REMOVED = "NODE REMOVED";
+
+    public static boolean clientSaysGoodBye(String command) {
+        return command.equals(CMD_CLIENT_BYE);
     }
 
-    public static String serverSaysGoodBye() {
-        return SERVER_BYE;
+    public static String getServerGreeting(final Session session) {
+        return format(RSP_SERVER_INTRO, session.getSessionID());
+    }
+
+    public static String getServerGoodbye(final Session session) {
+        return format(RSP_SERVER_BYE, session.getClientName(), session.getDuration());
     }
 
     public static String getResponse(String command, Session session, Graph graph) {
 
-        if (command.startsWith(HI_I_M)) {
-            final String clientID = command.replace(HI_I_M, "");
+        if (command.startsWith(CMD_CLIENT_GREETING)) {
+            final String clientID = command.replace(CMD_CLIENT_GREETING, "");
             session.setClientName(clientID);
-            return format(HI, clientID);
-        } else if (command.equals(CLIENT_BYE)) {
+            return format(RSP_SERVER_GREETING, clientID);
+        } else if (command.equals(CMD_CLIENT_BYE)) {
             session.terminate();
-            return format(SERVER_BYE, session.getClientName(), session.getDuration());
+            return format(RSP_SERVER_BYE, session.getClientName(), session.getDuration());
         } else if (command.startsWith(CMD_ADD_NODE)) {
             String nodeName = command.replace(CMD_ADD_NODE, "");
             try {
                 graph.addNode(nodeName);
             } catch (NodeAlreadyExistsException e) {
-                return ERROR_NODE_ALREADY_EXISTS;
+                return RSP_ERROR_NODE_ALREADY_EXISTS;
             }
 
-            return NODE_ADDED;
+            return RSP_NODE_ADDED;
         } else if (command.startsWith(CMD_ADD_EDGE)) {
             String edge = command.replace(CMD_ADD_EDGE, "");
             String[] s = edge.split(" ");
             try {
-                graph.addEdge(s[0], s[1], Integer.parseInt(s[2]));
+                int weight = Integer.parseInt(s[2]);
+                graph.addEdge(s[0], s[1], weight);
 
             } catch (NodeNotFoundException e) {
-                return ERROR_NODE_NOT_FOUND;
+                return RSP_ERROR_NODE_NOT_FOUND;
             }
 
             return RSP_EDGE_ADDED;
@@ -64,7 +73,7 @@ public class CommunicationManager {
             try {
                 graph.removeNode(node);
             } catch (NodeNotFoundException e) {
-                return ERROR_NODE_NOT_FOUND;
+                return RSP_ERROR_NODE_NOT_FOUND;
             }
 
             return RSP_NODE_REMOVED;
@@ -74,7 +83,7 @@ public class CommunicationManager {
             try {
                 graph.removeEdge(s[0], s[1]);
             } catch (NodeNotFoundException e) {
-                return ERROR_NODE_NOT_FOUND;
+                return RSP_ERROR_NODE_NOT_FOUND;
             }
 
             return RSP_EDGE_REMOVED;
@@ -85,7 +94,7 @@ public class CommunicationManager {
             try {
                 shortestPath = graph.getShortestPath(s[0], s[1]);
             } catch (NodeNotFoundException e) {
-                return ERROR_NODE_NOT_FOUND;
+                return RSP_ERROR_NODE_NOT_FOUND;
             }
 
             return format("%d", shortestPath);
@@ -97,13 +106,13 @@ public class CommunicationManager {
             try {
                 closerNodes = graph.findNodesCloserThan(Integer.parseInt(s[0]), s[1]);
             } catch (NodeNotFoundException e) {
-                return ERROR_NODE_NOT_FOUND;
+                return RSP_ERROR_NODE_NOT_FOUND;
             }
 
             return closerNodes;
 
         }
 
-        return SERVER_UNKNOWN_REQUEST;
+        return RSP_UNKNOWN_REQUEST;
     }
 }
