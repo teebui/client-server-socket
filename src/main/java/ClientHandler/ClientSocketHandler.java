831e5b8fc2e9db924720f7c8e1ac2dc4b2fe23ea
Tuan-Anh BUI <tuananh.bui@euranova.eu>
2018-12-28 00:44:36 +0100

    Put java doc for ClientSocketHandler

--- src/main/java/ClientHandler/ClientSocketHandler.java
+++ src/main/java/ClientHandler/ClientSocketHandler.java
@@ -2,7 +2,6 @@ package ClientHandler;
 
 import CommunicationHandler.CommunicationManager;
 import Graph.Graph;
-import Server.TCPServer;
 import org.apache.logging.log4j.LogManager;
 
 import java.io.BufferedReader;
@@ -11,11 +10,12 @@ import java.io.InterruptedIOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.logging.Logger;
 
-import static java.lang.String.*;
 import static java.lang.String.format;
 
+/**
+ * Runs as a thread and takes care of conversation between the server and a client connected
+ */
 public class ClientSocketHandler implements Runnable {
 
     private static final org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger(ClientSocketHandler.class);
@@ -23,25 +23,29 @@ public class ClientSocketHandler implements Runnable {
     private static final String CLIENT_SAYS = "Client: %s";
     private static final String SERVER_SAYS = "Server: %s";
     private static final String MSG_TIMED_OUT = "Timed out.";
+    private static final String MSG_ERROR_READING_CLIENT_COMMAND = "Error reading client's command...";
+    private static final String MSG_ERROR_TERMINATE_CLIENT_SOCKET = "Error terminating client socket...";
+    private static final String MSG_ERROR_INITIALISING_CLIENT_SOCKET = "Error initialising client socket...";
 
     private final Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
     private CommunicationManager comm;
 
-    public ClientSocketHandler(final Socket clientSkt, final Graph graph) throws IOException {
+    public ClientSocketHandler(final Socket clientSkt, final Graph graph) {
         clientSocket = clientSkt;
         comm = new CommunicationManager(new Session(), graph);
-        initializeClientSocket();
+        initialize();
     }
 
+    @Override
     public void run() {
         sendResponse(comm.getServerGreeting());
         LOGGER.info(format("Server: %s", comm.getServerGreeting()));
 
         String command, response;
         try {
-            // Read client's command and look for the right response
+            // Reads client's command and looks for the right response
             // until the client wants to stop the conversation
             while ((command = in.readLine()) != null) {
                 LOGGER.info(format(CLIENT_SAYS, command));
@@ -57,19 +61,19 @@ public class ClientSocketHandler implements Runnable {
             LOGGER.debug(MSG_TIMED_OUT, e);
             sendResponse(comm.getServerGoodbye());
         } catch (final IOException e) {
-            LOGGER.error("Error reading client's command...", e);
+            LOGGER.error(MSG_ERROR_READING_CLIENT_COMMAND, e);
         } finally {
-            terminateClient();
+            terminate();
         }
     }
 
-    private void terminateClient() {
+    private void terminate() {
         try {
             clientSocket.close();
             in.close();
             out.close();
         } catch (final IOException e) {
-            LOGGER.error("Error terminating client socket handler...", e);
+            LOGGER.error(MSG_ERROR_TERMINATE_CLIENT_SOCKET, e);
         }
     }
 
@@ -77,9 +81,13 @@ public class ClientSocketHandler implements Runnable {
         out.println(response);
     }
 
-    private void initializeClientSocket() throws IOException {
-        clientSocket.setSoTimeout(CLIENT_TIMEOUT);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
+    private void initialize()  {
+        try {
+            clientSocket.setSoTimeout(CLIENT_TIMEOUT);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            LOGGER.error(MSG_ERROR_INITIALISING_CLIENT_SOCKET, e);
+        }
     }
 }
